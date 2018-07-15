1de2be688e9d513c19f8195e2217977e82630732
David Lyon <david.lyon@clixx.io>
2018-07-15 20:42:17 +1000

    Updated to show Parts Directories

--- src/settingsdialog.cpp
+++ src/settingsdialog.cpp
@@ -1,11 +1,22 @@
+#include <QSettings>
+
 #include "settingsdialog.h"
 #include "ui_settingsdialog.h"
+
+#include "clixxiotprojects.h"
+#include "mainwindow.h"
 
 SettingsDialog::SettingsDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::SettingsDialog)
 {
     ui->setupUi(this);
+
+    MainWindow *mainwindow = (MainWindow *) getMainWindow();
+
+    ui->UserPartsDirlineEdit->setText(mainwindow->Projects->getUserLibraryDir());
+    ui->kayeiotPartsDirlineEdit->setText(mainwindow->Projects->getKayeIoTLibraryDir());
+
 }
 
 SettingsDialog::~SettingsDialog()
